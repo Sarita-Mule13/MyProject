f2c3690024f79da39d78a5f3b30d425940a8e6c1
sarita <saritamule13@gmail.com>
2024-03-29 19:45:36 +0530

    My first commit...

---
+++ Day1/FirstJavaProgram.java
@@ -0,0 +1,13 @@
+package Day1;
+
+public class FirstJavaProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method s
+	 
+		System.out.println("Welcome to Java");
+		System.out.println(10+30);
+		
+	}
+
+}
